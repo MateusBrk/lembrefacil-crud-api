6ce3d438576632837f65939f174a1d706bf388b9
Igor Oliveira <igornutrietec2020@gmail.com>
2024-11-28 23:59:28 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,19 +1,21 @@
 <?php
-
-ini_set('mysqli.connect_timeout', 30);  // 30 segundos de timeout para a conexão
-ini_set('default_socket_timeout', 30);  // 30 segundos de timeout para sockets
 // Carregar o arquivo .env
 require_once __DIR__ . '/vendor/autoload.php'; // Caminho correto para o autoload do Composer
 Dotenv\Dotenv::createImmutable(__DIR__)->load();
 
 // Obter as variáveis de ambiente
-$dbHost = $_ENV['DB_HOST'];
-$dbName = $_ENV['DB_NAME'];
+$dbHost = $_ENV['DB_HOST'];  // Exemplo: junction.proxy.rlwy.net
+$dbPort = $_ENV['DB_PORT'];  // Exemplo: 23851
 $dbUser = $_ENV['DB_USER'];
 $dbPass = $_ENV['DB_PASSWORD'];
+$dbName = $_ENV['DB_NAME'];
 
-// Conexão com o banco de dados
-$conexao = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName);
+// Definir tempo de espera para a conexão e socket
+ini_set('mysqli.connect_timeout', 30);  // Timeout de 30 segundos para a conexão
+ini_set('default_socket_timeout', 30);  // Timeout de 30 segundos para operações de rede
+
+// Conexão com o banco de dados, incluindo a porta
+$conexao = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName, $dbPort);
 
 if (!$conexao) {
     die('Erro na conexão: ' . mysqli_connect_error());
